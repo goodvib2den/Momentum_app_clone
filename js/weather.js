7b8d7d80b83ca3d2049419f6361c9d2c4bdbb22b
eden <gmean7@gmail.com>
2021-11-10 12:03:49 +0900

    Add weather commit

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+// strings variable
+const API_KEY = "40232206e6bfcfc7b00b1f8dae4714ef";
+
+// geolocation allow fn
+function onGeo(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = data.weather[0].main;
+      city.innerText = data.name;
+    });
+}
+
+// geolocation err fn
+function onGeoErr() {
+  alert("현재 위치 정보를 확인 할 수 없습니다.");
+}
+
+// geo fn
+navigator.geolocation.getCurrentPosition(onGeo, onGeoErr);
