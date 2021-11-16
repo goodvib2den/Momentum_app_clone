5027131068915a7c992212fd28cf676e14c4e9e9
eden <gmean7@gmail.com>
2021-11-16 13:00:45 +0900

    fix js

--- js/background.js
+++ js/background.js
@@ -5,4 +5,4 @@ const imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 const imgSelec = imgs[Math.floor(Math.random() * imgs.length)];
 
 // print bg img
-document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url("../img/${imgSelec}")`;
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url("/img/${imgSelec}")`;
