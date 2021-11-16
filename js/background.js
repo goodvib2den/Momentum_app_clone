fd960377f4023db78326652d423648ac1598e291
eden <gmean7@gmail.com>
2021-11-16 13:34:33 +0900

    add img api

--- js/background.js
+++ js/background.js
@@ -1,8 +1,2 @@
-// img empty array
-const imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
-
-// random imgs
-const imgSelec = imgs[Math.floor(Math.random() * imgs.length)];
-
 // print bg img
-document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url("/image/${imgSelec}")`;
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url("https://source.unsplash.com/random")`;
