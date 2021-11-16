38222be237c932e85b2b847dbbdefeb88e9fd3e6
eden <gmean7@gmail.com>
2021-11-16 13:44:57 +0900

    Bg img api test

--- js/background.js
+++ js/background.js
@@ -1,2 +1,2 @@
 // print bg img
-document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url("https://source.unsplash.com/random")`;
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url("https://source.unsplash.com/1920x1080/?film")`;
